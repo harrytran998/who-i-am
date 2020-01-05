882466bd38eee73d036b0fcc13983b837f7542e3
Demons Light <jetaimefrc@gmail.com>
2020-01-05 13:14:58 +0700

    feat: add sidebar for nav items

---
+++ docs/.vuepress/link/sidebar.js
@@ -0,0 +1,50 @@
+module.exports = {
+  getLeadershipSideBar() {
+    return [
+      {
+        title: 'Introduction',
+        children: [['', 'Giới thiệu']],
+      },
+      {
+        title: '...',
+        children: ['..'],
+      },
+    ]
+  },
+  getFinanceSideBar() {
+    return [
+      {
+        title: 'Introduction',
+        children: [['', 'Giới thiệu']],
+      },
+      {
+        title: '...',
+        children: ['..'],
+      },
+    ]
+  },
+  getInvestingSideBar() {
+    return [
+      {
+        title: 'Introduction',
+        children: [['', 'Giới thiệu']],
+      },
+      {
+        title: '...',
+        children: ['..'],
+      },
+    ]
+  },
+  getOtherSideBar() {
+    return [
+      {
+        title: 'Introduction',
+        children: [['', 'Giới thiệu']],
+      },
+      {
+        title: '...',
+        children: ['..'],
+      },
+    ]
+  },
+}
