d9fbd7bdb3207ddba04a37ece9c7f1fa8c14ab81
Demons Light <jetaimefrc@gmail.com>
2020-01-05 13:14:36 +0700

    feat: Add navbar for app

---
+++ docs/.vuepress/link/nav.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    text: 'Quản trị - Lãnh đạo',
+    link: '/leadership/',
+  },
+  {
+    text: 'Tài chính',
+    link: '/finance/',
+  },
+  {
+    text: 'Đầu tư',
+    link: '/investing/',
+  },
+  {
+    text: 'Other',
+    ariaLabel: 'Other',
+    items: [
+      {
+        text: 'Sức khoẻ cho dân IT',
+        items: [
+          {
+            text: 'Chế độ ăn uống',
+            link: '/other/health/eating-habits.html',
+          },
+          {
+            text: 'Workout',
+            link: '/other/health/workout.html',
+          },
+          {
+            text: 'Chạy bộ',
+            link: '/api/cli.html',
+          },
+        ],
+      },
+      {
+        text: '...',
+        items: [
+          {
+            text: '....',
+            link: '...',
+          },
+        ],
+      },
+    ],
+  },
+]
